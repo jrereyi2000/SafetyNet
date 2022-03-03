e675b5725d44fd6f1b24bb37a28c3083fdc718a8
JR <jrereyi@stanford.edu>
2022-03-02 16:43:51 -0800

    Final Team Page

---
+++ SafetyNet_Team_Page/SafetyNet_files/extract-css-runtime-13ea7a8552a69cc61c748-min.en-US.js
@@ -0,0 +1,2 @@
+(function(){"use strict";var t={}})();
+//# sourceMappingURL=https://sourcemaps.squarespace.net/universal/scripts-compressed/extract-css-runtime-fc46c3221516448ecd855-min.en-US.js.map
---
+++ SafetyNet_Team_Page/index.js
@@ -0,0 +1,5 @@
+window.onresize = (ev) => {
+    if (window.innerWidth < 768) console.log('phone')
+}
+
+console.log("Script Loaded")
